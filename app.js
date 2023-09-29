631d97d19915edb9edf08b063b226d1254c4d014
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-09-29 12:02:56 +0500

    Simplified the HTML, used fixed width font for the color code. Added the event handler on the button.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello world!')
+function randHexDigit() {
+    const rand16 = Math.floor(Math.random()*16)
+    return rand16.toString(16)
+}
+
+const sp = document.getElementById('color')
+setRandColor()
+
+const btn = document.getElementById('btn')
+btn.addEventListener('click', setRandColor)
+
+function randColor() {
+    return '#' + randHexDigit() + randHexDigit() + randHexDigit() + randHexDigit() + randHexDigit() + randHexDigit()
+}
+
+function setRandColor() {
+    const color = randColor()
+    sp.textContent = color
+    document.body.style.background = color
+}
